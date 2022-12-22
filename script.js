5d61f747486e9437564a84000c1158738ec1226e
umidkhan <umidxondeveloper@mail.ru>
2022-12-22 18:09:59 +0500

    fix

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 let openNav = document.querySelector('.openNavBtn');
-let navLink = document.querySelector('.navLinks');
+let navLink = document.querySelector('.openedNavLinks');
 
-openNav.addEventListener("click", (e) => {
+function OpenNav() {
+    openNav.addEventListener("click", (e) => {
     console.log('Nav opened');
     openNav.style.borderColor = "blue"
-    openNav.style.border = "solid"
-    navLink.style.display = "block"
+    navLink.style.marginTop = "62px"
+    navLink.style.transition = "2s"
 })
+}
+OpenNav()
+
+// openNav.addEventListener("click", (e) => {
+//     // OpenNav.preventDefault()
+//     console.log('Nav closed');
+//     openNav.style.borderColor = "blue"
+//     navLink.style.marginTop = "-100px"
+//     navLink.style.transition = "2s"
+// })
 
 /* openNav.addEventListener("mouseleave", () => {
     openNav.style.border = "none"
